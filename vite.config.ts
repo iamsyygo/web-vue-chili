b2cf5c8324ad704af1422a1845390d368a63fdbe
iamsyygo <2683030687@qq.com>
2024-07-24 00:32:32 +0800

    ✨ 添加加载动画和样式
    为页面添加了一个加载动画和相应的样式，以提升用户体验。

--- vite.config.ts
+++ vite.config.ts
@@ -69,6 +69,9 @@ export default defineConfig({
           title: packageData.name,
           version: packageData.version,
           logo: '/logo.png',
+          loadingHtml:
+            '<svg class="init-loading"><g><path d="M 50,100 A 1,1 0 0 1 50,0"/></g><g><path d="M 50,75 A 1,1 0 0 0 50,-25"/></g><defs><linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%"><stop offset="0%" style="stop-color:#FF56A1;stop-opacity:1" /><stop offset="100%" style="stop-color:#FF9350;stop-opacity:1" /></linearGradient></defs></svg>',
+          loadingStyle: `<style> .init-loading { overflow: visible; width: 100px; height: 150px; g { animation: slide 2s linear infinite; &:nth-child(2) { animation-delay: 2s / 4; path { animation-delay: 2s / 4; stroke-dasharray: 0px 157px + 1; stroke-dashoffset: 1px; } } } path { stroke: url(#gradient); stroke-width: 20px; stroke-linecap: round; fill: none; stroke-dasharray: 0 157px; stroke-dashoffset: 0; animation: escalade 2s cubic-bezier(0.8, 0, 0.2, 1) infinite; } } @keyframes slide { 0% { transform: translateY(-50px); } 100% { transform: translateY(50px); } } @keyframes escalade { 0% { stroke-dasharray: 0 157px; stroke-dashoffset: 0; } 50% { stroke-dasharray: 157px 157px; stroke-dashoffset: 0; } 100% { stroke-dasharray: 157px 157px; stroke-dashoffset: -(157px - 1); } } </style>`,
         },
         tags: [
           { tag: 'script', attrs: { src: '/iconfont.js' } },
